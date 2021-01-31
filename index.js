74448b1265cb2c9f010965405b16c95d37ed171b
Harender Kumar <harender094@gmail.com>
2021-01-31 09:18:02 +0530

    Project fetching code

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+const ul = document.getElementById('git-api')
+
+function createNade(element){
+  return document.createElement(element);
+}
+
+function append(parent, child){
+  return parent.appendChild(child)
+}
+
+fetch('git.json')
+.then((resp) => resp.json)
+.then(function (data){
+  let git = data.Projects;
+  return git.map(function (project){
+    let li = createNade('li')
+    let name = createNade('name')
+
+    name.innerHTML = `${git.name}`
+
+    append(li, name)
+    append(ul, li)
+  })
+
+  .catch(function (error){
+    console.log(error)
+  })
+})
